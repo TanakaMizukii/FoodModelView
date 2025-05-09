b31ab3fbf50499e12b115e645ef4ed71654c9d61
ryusei <ryusei25110@icloud.com>
2025-05-09 18:32:33 +0900

    HTML変更、ARButton変更に苦戦中

--- Android_FoodModel/Three.js
+++ Android_FoodModel/Three.js
@@ -2,7 +2,6 @@ import * as THREE from "three";
 import { ARButton } from "three/addons/webxr/ARButton.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
-let container;
 let scene, camera, light, renderer;
 let controller;
 
@@ -12,8 +11,6 @@ let reticle;
 let hitTestSource = null;
 // ヒットテストをリクエストしていないかどうかを確認するための変数
 let hitTestSourceRequested = false;
-
-init();
 
 async function init() {
     const width = window.innerWidth;
@@ -42,8 +39,8 @@ async function init() {
     // 「ユーザーの周囲の平面を検出する」機能をオンにしているこれによりframe.getHitTestResults()を有効化
     const arButton = (ARButton.createButton(renderer, {
         requiredFeatures: ['hit-test'],
-        optionalFeatures: ['dom-overlay', 'dom-overlay-for-handle-ar' ],
-        domOverlay: { root: document.body }
+        optionalFeatures: ['dom-overlay', 'dom-overlay-for-handle-ar' ], // モバイルでのUIの実装のために記述
+        domOverlay: { root: document.body } // どの要素をオーバーレイ領域として扱うかを指定
     }));
     document.body.appendChild( arButton );
 
@@ -82,34 +79,18 @@ async function init() {
     scene.add(controller);
 }
 
-/* animate 関数は、WebGLRendererのsetAnimationLoopによって
-毎フレーム呼ばれ、その際にtimestamp, frameの二つの引数が渡される
-timestampおよび performance.now()はページが読み込まれてからの経過時間を、
-frameは、XRFrameオブジェクトであり、そのフレームにおけるWebXRの様々な状態情報を提供する。 */
+init();
+
 function animate(timestamp, frame) {
     if (frame) {
-        // rendererReferenceSpaceはXRコンテンツの座標系（カメラが何処に在って、何処を向いているのか）を表すオブジェクト
-        // 例えるなら、現実世界の「地図上の座標」のようなもので、どこに何かを置くかの基準となる場所。
         const rendererReferenceSpace = renderer.xr.getReferenceSpace();
-        // 現在実行中のAR体験そのものを指す。
-        // 例えるなら、AR体験が始まっている「部屋」や「セッション」のようなもので、その中でユーザーの動きやカメラの情報、入力デバイスの状態などを管理している。
-        // この session を使って、例えばヒットテストの要求やデバイスからの入力を扱う。
         const session = renderer.xr.getSession();
 
         if (hitTestSourceRequested === false) {
-            // viewerReferenceSpace は取得した座標系（カメラが何処に在って、何処を向いているのか）の情報で、後でヒットテストに使う。
-            /* .thenは左側で成功した物を右側の引き数として使用する！ */
             session.requestReferenceSpace('viewer').then( function (viewerReferenceSpace) {
-                // ヒットテストソースとは、ユーザーの環境内にある平面や表面などを検出するためのデータを取得するためのオブジェクト
-                // 例えば、ARで「どこにオブジェクトを置くか」を判断するために、ユーザーの周りに実際に存在する床やテーブルなどの平面情報を得るために使われる
-                // この処理も Promise を返すので、.then(function (source) {...}) により、成功した場合に取得したソースを処理
                 session.requestHitTestSource({ space: viewerReferenceSpace }).then( function (source) {
-                    // ここで、Promise が返したヒットテストソース（source）を、グローバル変数 hitTestSource に保存
                     hitTestSource = source;
                 });
-                // session.requestHitTestSource({ space: referenceSpace })が成功すると、WebXR API は XRHitTestSource オブジェクトを返す。
-                // この操作によって次からはframe.getHitTestResults(hitTestSource) を使うことでヒットテストの結果の配列を取得できるようになる。
-                // frame.getHitTestResultsはユーザーの環境内に存在する平面（床、テーブルなど）との交差情報を取得できる「窓口」のような存在となる。
             });
 
             hitTestSourceRequested = true;
